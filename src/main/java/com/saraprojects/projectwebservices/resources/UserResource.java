2ffe46b40f470353b0bb56c068446cb3e760d0cc
Sara <sara.s.mageste@gmail.com>
2025-09-18 10:12:40 -0300

    User entity and resource

---
+++ src/main/java/com/saraprojects/projectwebservices/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.saraprojects.projectwebservices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saraprojects.projectwebservices.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User (1L, "Maria", "maria@gmail.com", "99999999", "000000");
+		return ResponseEntity.ok().body(u);
+	}
+}
