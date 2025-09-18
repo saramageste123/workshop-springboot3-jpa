7ed06b63a710c23aea23767bcff2d40cee3addfc
Sara <sara.s.mageste@gmail.com>
2025-09-18 09:58:40 -0300

    Project created

---
+++ src/main/java/com/saraprojects/projectwebservices/ProjectwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.saraprojects.projectwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saraprojects/projectwebservices/ProjectwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraprojects.projectwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
